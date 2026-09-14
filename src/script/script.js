2cd245d4f19b58bb3863b67aa0634c082dbe5056
PPinheiro20 <pedro.h.senai@aluno.senai.br>
2026-09-14 13:47:44 -0300

    feat: main concluida com adição de sections

---
+++ src/script/script.js
@@ -0,0 +1,8 @@
+// Importa o CSS do Bootstrap instalado pelo npm
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Importa o JavaScript do Bootstrap
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Importa o CSS personalizado
+import "./style.css";
