a8ef07f1383da20882bedb0e33ac19b2b90202dc
PPinheiro20 <pedro.h.senai@aluno.senai.br>
2026-09-14 14:17:33 -0300

    feat: códigos do css concluídos. fix: correção da lincagem dos arquivos, retirados os imports do script.js e adicionado a lincagem correta na index.html.

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,1 @@
-// Importa o CSS do Bootstrap instalado pelo npm
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// Importa o JavaScript do Bootstrap
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-// Importa o CSS personalizado
-import "../style/style.css"
+console.log("Site carregado!");
